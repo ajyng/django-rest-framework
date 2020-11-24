5fb5b1552655ff822d843c6c97868f44e69092a1
ajyng <zxzimin@naver.com>
2020-11-25 03:15:36 +0900

    APIView를 활용한 뷰 만들기

--- instagram/serializers.py
+++ instagram/serializers.py
@@ -8,15 +8,17 @@ class AuthorSerializer(serializers.ModelSerializer):
         fields = ['username', 'email']
 
 class PostSerializer(serializers.ModelSerializer):
-    # username = serializers.ReadOnlyField(source='author.username')
-    author = AuthorSerializer()
+    author_username = serializers.ReadOnlyField(source='author.username') # 조회할 때만 사용 가능하다!
+    # author = AuthorSerializer()
     
     class Meta:
         model = Post
         fields = [
             'pk',
-            'author',
+            # 'author',
+            'author_username',
             'message',
             'created_at',
             'updated_at',
+            'is_public',
         ]
--- instagram/urls.py
+++ instagram/urls.py
@@ -6,6 +6,6 @@ router = DefaultRouter()
 router.register('post', views.PostViewSet) # 2개의 URL을 만들어준다.
 
 urlpatterns = [
-    path('public/', views.PublicPostListAPIView.as_view()),
+    path('public/', views.public_post_list),
     path('', include(router.urls)),
 ]
