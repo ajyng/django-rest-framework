759bd7b8671cb64f9caa0e31776669f71faf2f06
ajyng <zxzimin@naver.com>
2020-11-25 02:36:04 +0900

    JSON 응답뷰 만들기(DRF 구현)

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,4 +1,10 @@
+from rest_framework.routers import DefaultRouter
+from . import views
+from django.urls import path, include
+
+router = DefaultRouter()
+router.register('post', views.PostViewSet) # 2개의 URL을 만들어준다.
 
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
